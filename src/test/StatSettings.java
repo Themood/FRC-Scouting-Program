1b7f6735beb7cd4c04e005e2aba77909f47944df
Themood <mahmoodgladney@gmail.com>
2017-05-29 01:05:04 -0700

    Added Settings and Ranks

---
+++ src/test/StatSettings.java
@@ -0,0 +1,15 @@
+package test;
+
+public class StatSettings {
+
+//This sets a bunch of variables so i don't use magic numbers 
+		//this sets the event you are you at, to set, use the year follow by the event code.
+		//example 2017code for colorado regional, 2017azfl for arizona north 
+		public static String eventcode = "2017azfl"; 
+		//this set how many teams are at the event so it can request the ranks 
+		//NOTE: MAKE SURE YOU SEE IF ANY TEAMS ARE A NO SHOW, SOMETIMES A TEAM WILL BE REGISTERED BUT NOT SHOW UP AT THE EVENT 
+		public static int numberofteams = 48;  
+
+	}
+
+
--- src/test/testone.java
+++ src/test/testone.java
@@ -8,15 +8,17 @@ import com.cpjd.main.TBA;
 import com.cpjd.models.Team;
 import com.cpjd.models.Event;
 import com.cpjd.models.Match;
+import test.StatSettings;
+
 
 public class testone {
 
 	private static Scanner user_input;
 	public static int teamNumber;
-	public static String eventinput;
+	//public static String eventcode;
 
 	public static void main(String[] args) throws FileNotFoundException {
-		// TODO Auto-generated method stub
+	
 
 		// Get everything set up
 		TBA.setID("Data Buckets", "Scouting app", "V.01");
@@ -25,18 +27,12 @@ public class testone {
 		boolean inputloop = true;
 		Settings.GET_EVENT_MATCHES = true;
 		Settings.GET_EVENT_ALLIANCES = true;
+		Settings.FIND_TEAM_RANKINGS = true;
 		while (inputloop = true) {
-			// Print some information
-
-			// event info
-			// System.out.println(Event.teams);
-
 			String command = user_input.next();
+			
 			if (command.equalsIgnoreCase("Team")) {
-
 				teamNumber = user_input.nextInt();
-				
-				
 				Team team = tba.getTeam(teamNumber);
 				System.out.println(team.team_number);
 				System.out.println(team.website);
@@ -45,17 +41,21 @@ public class testone {
 				System.out.println(team.nickname);
 				System.out.println(team.rookie_year);
 				System.out.println(team.motto);
-			} else if (command.equalsIgnoreCase("Event")) {
-				eventinput = user_input.next();
-				Event event = new TBA().getEvent(eventinput);
-				System.out.println(event.start_date);
-
+			} 
+			else if (command.equalsIgnoreCase("Event")) {
+				//eventcode = user_input.next();
+				Event event = new TBA().getEvent(StatSettings.eventcode);
+				for (int i = 0; i < StatSettings.numberofteams; i++) {
+				System.out.println(event.teams[i].nickname+" Rank: "+event.teams[i].rank+" Score: "+event.teams[i].rankingScore);
+				}
+				//System.out.println(StatSettings.numberofteams);
+				//System.out.println(StatSettings.eventcode);
 				// System.out.println("WORK IN PROGRESS");
 			}
-
 			else {
 				System.out.println("Unknown Command");
 			}
 		}
-	}
-}
+	}}
+	
+
